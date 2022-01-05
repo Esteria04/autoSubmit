d5deb1ee517bf7ebf1170d5a5990da9724d7ab05
dev.esteria04 <67038945+areason04@users.noreply.github.com>
2022-01-05 10:31:33 +0900

    Add files via upload

---
+++ autoSubmit.js
@@ -0,0 +1,5 @@
+function autoSubmit(a) {
+    setTimeout(function () {
+        document.querySelector(".office-form-theme-primary-background.office-form-theme-button.office-form-bottom-button.button-control.light-background-button.__submit-button__").click();
+    }, a*60000);
+    }
